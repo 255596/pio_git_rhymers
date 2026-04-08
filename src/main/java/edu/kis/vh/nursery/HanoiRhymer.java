b3dc653b56fa36ee1e1a02d60668aa9d8f4052cc
Kacper <255596@edu.p.lodz.pl>
2026-04-08 13:06:12 +0200

    17. Wygenerowanie i uzupełnienie dokumentacji dla klasy HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,13 +1,27 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa HanoiRhymer rozszerza domyślną wyliczankę.
+ * Jej działanie polega na tym, że odrzuca liczby,
+ * które są większe od liczby, która znajduje sie na szczycie.
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private int totalRejected = 0;
-
+    /**
+     * Metoda zwracająca całkowitą liczbę odrzuconych prób dodania elementu.
+     * liczba odrzuconych elementów to wartość zmiennej totalRejected
+     */
     public int reportRejected() {
         return totalRejected;
     }
 
+    /**
+     * Metoda dodająca nową liczbę do wyliczanki.
+     * Jeśli wyliczanka nie jest pusta i wprowadzana liczba jest większa
+     * od tej na szczycie, liczba zostaje odrzucona, a licznik odrzuceń wzrasta.
+     * W przeciwnym razie liczba jest dodawana normalnie.
+     */
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > peek())
